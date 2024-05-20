72495b406558aa24f2477a603d720f367e1b7727
Zak Ward <zakward85@gmail.com>
2024-05-20 12:49:47 -0500

    changes to colored boxes

--- react/flexbox-workshop/src/App.jsx
+++ react/flexbox-workshop/src/App.jsx
@@ -7,32 +7,55 @@ function App() {
   return (
     <>
       <div className="App">
-        <div className="flexbox-container learning-objectives">
-          <h1>Learning Objectives</h1>
-          <ul>
-            <li>Explain the basic concepts of CSS flexbox layout</li>
-            <li>Implement flexbox in your React and Vanilla JS projects</li>
-            <li>Arrange elements horizontally and vertically using flexbox properties</li>
-          </ul>
+        <h1>Flex Box Workshop</h1>
+        <h2>flex-direction</h2>
+        <div className="flexbox-container direction">
+          <p className="one"> 1</p>
+          <p className="two"> 2</p>
+          <p className="three"> 3</p>
+          <p className="four"> 4</p>
+          <p className="five"> 5</p>
         </div>
-        <div className='flexbox-container what-is-flexbox'>
-          <h1>What is FLEXBOX?</h1>
-          <ul>
-            <li>Flexbox is a css layout model that allows you to easily arrange elements horizontally and vertically.</li>
-            <li>It provides more control over the placement of elements compared to traditional methods.</li>
-            <li>Flexbox is responsive, meaning your layouts will adapt to different screen sizes.</li>
-          </ul>
+        <h2>flex-wrap</h2>
+        <div className='flexbox-container wrap'>
+          <p className="one"> 1</p>
+          <p className="two"> 2</p>
+          <p className="three"> 3</p>
+          <p className="four"> 4</p>
+          <p className="five"> 5</p>
         </div>
-
-        <div className="flexbox-container">
-          <h1>Row Practice</h1>
-          <ul className="flex-row">
-            <li>element 1</li>
-            <li>element 2</li>
-            <li>element 3</li>
-            </ul>
+        <h2>flex-flow</h2>
+        <div className='flexbox-container flow'>
+          <p className="one"> 1</p>
+          <p className="two"> 2</p>
+          <p className="three"> 3</p>
+          <p className="four"> 4</p>
+          <p className="five"> 5</p>
         </div>
-
+        <h2>justify-content // align-items</h2>
+        <div className='flexbox-container justify'>
+          <p className="one"> 1</p>
+          <p className="two"> 2</p>
+          <p className="three"> 3</p>
+          <p className="four"> 4</p>
+          <p className="five"> 5</p>
+        </div>
+        <h2>gap</h2>
+        <div className='flexbox-container gap'>
+          <p className="one"> 1</p>
+          <p className="two"> 2</p>
+          <p className="three"> 3</p>
+          <p className="four"> 4</p>
+          <p className="five"> 5</p>
+        </div>
+        <h2>align-self</h2>
+        <div className='flexbox-container align'>
+          <p className="one"> 1</p>
+          <p className="two"> 2</p>
+          <p className="three"> 3</p>
+          <p className="four"> 4</p>
+          <p className="five"> 5</p>
+        </div>
       </div>
     </>
   )
