e615ac34ee708d50b7fe44aef72b3b29609e5f8e
Zak Ward <zakward85@gmail.com>
2024-05-21 13:20:10 -0500

    finishing touches

--- react/flexbox-workshop/src/App.jsx
+++ react/flexbox-workshop/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <div className="App">
         <h1>Flex Box Workshop</h1>
         <h2>flex-direction</h2>
+        <p style = {{width: "100%"}}>Controls the main axis of flex items within the container</p>
         <div className="flexbox-container direction">
           <p className="one"> 1</p>
           <p className="two"> 2</p>
@@ -17,6 +18,7 @@ function App() {
           <p className="five"> 5</p>
         </div>
         <h2>flex-wrap</h2>
+        <p style = {{width: "100%"}}>Defines whether flex items wrap onto multiple lines if they don't fit within the container </p>
         <div className='flexbox-container wrap'>
           <p className="one"> 1</p>
           <p className="two"> 2</p>
@@ -25,6 +27,8 @@ function App() {
           <p className="five"> 5</p>
         </div>
         <h2>flex-flow</h2>
+        <p style = {{width: "100%"}}>(Shorthand) Combines flex-direction and flex-wrap properties into one </p>
+
         <div className='flexbox-container flow'>
           <p className="one"> 1</p>
           <p className="two"> 2</p>
@@ -33,6 +37,9 @@ function App() {
           <p className="five"> 5</p>
         </div>
         <h2>justify-content // align-items</h2>
+        <p style = {{width: "100%"}}>Justify Content: Distributes flex items along the main axis of the container </p>
+        <p style = {{width: "100%"}}>Align Items:  Aligns flex items along the cross axis of the container </p>
+
         <div className='flexbox-container justify'>
           <p className="one"> 1</p>
           <p className="two"> 2</p>
@@ -41,6 +48,7 @@ function App() {
           <p className="five"> 5</p>
         </div>
         <h2>gap</h2>
+        <p style = {{width: "100%"}}>Creates spacing between flex items in a row or column</p>
         <div className='flexbox-container gap'>
           <p className="one"> 1</p>
           <p className="two"> 2</p>
@@ -49,6 +57,8 @@ function App() {
           <p className="five"> 5</p>
         </div>
         <h2>align-self</h2>
+        <p style = {{width: "100%"}}>Allows individual flex items to override the default align-items value for the container</p>
+
         <div className='flexbox-container align'>
           <p className="one"> 1</p>
           <p className="two"> 2</p>
